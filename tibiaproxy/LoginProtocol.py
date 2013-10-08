435f1a3dded25c329e9f76f5ec0b2acab8404fa0
d33tah <d33tah@gmail.com>
2013-10-08 18:27:02 +0200

    Parse the destination login server reply.

--- tibiaproxy/LoginProtocol.py
+++ tibiaproxy/LoginProtocol.py
@@ -3,6 +3,18 @@ from XTEA import XTEA
 from util import *
 
 
+class LoginCharacterEntry:
+    name = ""
+    world = ""
+    ip = 0
+    port = 0
+
+
+class LoginReply:
+    motd = ""
+    characters = []
+
+
 class LoginProtocol:
 
     def __init__(self, conn):
@@ -14,5 +26,26 @@ class LoginProtocol:
         self.k = [msg.getU32() for i in range(4)]
 
     def parseReply(self, msg):
-        msg.skipBytes(2)
-        decrypted = XTEA.decrypt(msg, self.k)
+        ret = LoginReply()
+
+        size = msg.getU16()
+        msg = XTEA.decrypt(msg, self.k)
+        assert(len(msg.getBuffer()) == size)
+        decrypted_size = msg.getU16()
+        assert(decrypted_size == size - 5)
+
+        packet_type = msg.getByte()
+        assert(packet_type == 0x14)
+        ret.motd = msg.getString()
+
+        assert(msg.getByte() == 0x64)
+        num_chars = msg.getByte()
+        for i in range(num_chars):
+            char = LoginCharacterEntry()
+            char.name = msg.getString()
+            char.world = msg.getString()
+            char.ip = msg.getU32()
+            char.port = msg.getU16()
+            ret.characters += [char]
+
+        return ret
