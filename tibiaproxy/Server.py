b8e0c21e9336a81ff64628c3596eb3cf6c6a7964
d33tah <d33tah@gmail.com>
2013-10-08 16:23:45 +0200

    Accept a connection.

--- tibiaproxy/Server.py
+++ tibiaproxy/Server.py
@@ -1,3 +1,6 @@
+import socket
+
+
 class Server:
 
     def __init__(self, destination_host, destination_port,
@@ -7,7 +10,15 @@ class Server:
         self.listen_host = listen_host
         self.listen_port = int(listen_port)
 
+        self.s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.s.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+        self.s.bind((self.listen_host, self.listen_port))
+
     def run(self):
         print(("Listening on address %s:%s, connections will be forwarded " +
                "to %s:%s") % (self.destination_host, self.destination_port,
                               self.listen_host, self.listen_port))
+
+        self.s.listen(1)
+        conn, addr = self.s.accept()
+        print("Received a connection from %s:%s" % addr)
