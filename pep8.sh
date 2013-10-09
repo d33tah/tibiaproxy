ab0c4a6e66f2eaf51dcc3fae38a731a0c677c973
d33tah <d33tah@gmail.com>
2013-10-09 17:16:16 +0200

    Add some documentation for pep8.sh.

--- pep8.sh
+++ pep8.sh
@@ -1,2 +1,11 @@
 #!/bin/sh
+
+# pep8.sh
+#
+# The Unix shell script I used in order to validate the Python source code of
+# the project for PEP-8 standard conformity.
+#
+# Usage:
+#   ./pep8.sh
+
 find -name '*.py' | tee /dev/stderr | xargs pep8
