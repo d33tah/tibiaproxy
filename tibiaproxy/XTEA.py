e9d7600e80ee8051d4339d16f536a6d28b16884d
d33tah <d33tah@gmail.com>
2013-10-26 23:11:10 +0200

    Get rid of numpy as a dependency.

--- tibiaproxy/XTEA.py
+++ tibiaproxy/XTEA.py
@@ -17,13 +17,48 @@ Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 """
 
 import struct
-import ctypes
-import numpy
 
 from NetworkMessage import NetworkMessage
 
 
-u = numpy.uint32
+class U32:
+    """Emulates 32-bit unsigned int known from C programming language."""
+
+    def __init__(self, num=0, base=None):
+        """Creates the U32 object.
+
+        Args:
+            num: the integer/string to use as the initial state
+            base: the base of the integer use if the num given was a string
+        """
+        if base is None:
+            self.int = int(num) % 2**32
+        else:
+            self.int = int(num, base) % 2**32
+
+    def __coerce__(self, x):
+        return None
+
+    def __str__(self):
+        return "<U32 instance at 0x%x, int=%d>" % (id(self), self.int)
+
+    def __getattr__(self, x):
+        # you might want to take a look here:
+        # http://stackoverflow.com/q/19611001/1091116
+        r = getattr(self.int, x)
+        if callable(r):  # return a wrapper if integer's function was requested
+            def f(*args, **kwargs):
+                if args and isinstance(args[0], U32):
+                    args = (args[0].int, ) + args[1:]
+                ret = r(*args, **kwargs)
+                if ret is NotImplemented:
+                    return ret
+                if x in ['__str__', '__repr__', '__index__']:
+                    return ret
+                ret %= 2**32
+                return U32(ret)
+            return f
+        return r
 
 
 class XTEA:
@@ -42,17 +77,17 @@ class XTEA:
         buf = msg.getRest()
         ret = ""
         for offset in range(len(buf)/8):
-            v0 = u(struct.unpack("<I", buf[offset*8:offset*8+4]))
-            v1 = u(struct.unpack("<I", buf[offset*8+4:offset*8+8]))
-            delta = u(0x61C88647)
-            sum = u(0)
+            v0 = U32(struct.unpack("<I", buf[offset*8:offset*8+4])[0])
+            v1 = U32(struct.unpack("<I", buf[offset*8+4:offset*8+8])[0])
+            delta = U32(0x61C88647)
+            sum = U32(0)
 
             for i in range(32):
-                v0 += ((v1 << u(4) ^ v1 >> u(5)) + v1) ^ \
-                      (sum + u(k[sum & u(3)]))
+                v0 += ((v1 << U32(4) ^ v1 >> U32(5)) + v1) ^ \
+                      (sum + U32(k[sum & U32(3)]))
                 sum -= delta
-                v1 += ((v0 << u(4) ^ v0 >> u(5)) + v0) ^ \
-                      (sum + u(k[sum >> u(11) & u(3)]))
+                v1 += ((v0 << U32(4) ^ v0 >> U32(5)) + v0) ^ \
+                      (sum + U32(k[sum >> U32(11) & U32(3)]))
 
             ret += struct.pack("<I", v0) + struct.pack("<I", v1)
         return NetworkMessage(ret, True)
@@ -70,17 +105,17 @@ class XTEA:
         buf = msg.getRest()
         ret = ""
         for offset in range(len(buf)/8):
-            v0 = u(struct.unpack("<I", buf[offset*8:offset*8+4]))
-            v1 = u(struct.unpack("<I", buf[offset*8+4:offset*8+8]))
-            delta = u(0x61C88647)
-            sum = u(0xC6EF3720)
+            v0 = U32(struct.unpack("<I", buf[offset*8:offset*8+4])[0])
+            v1 = U32(struct.unpack("<I", buf[offset*8+4:offset*8+8])[0])
+            delta = U32(0x61C88647)
+            sum = U32(0xC6EF3720)
 
             for i in range(32):
-                v1 -= ((v0 << u(4) ^ v0 >> u(5)) + v0) ^ \
-                      (sum + u(k[sum >> u(11) & u(3)]))
+                v1 -= ((v0 << U32(4) ^ v0 >> U32(5)) + v0) ^ \
+                      (sum + U32(k[sum >> U32(11) & U32(3)]))
                 sum += delta
-                v0 -= ((v1 << u(4) ^ v1 >> u(5)) + v1) ^ \
-                      (sum + u(k[sum & u(3)]))
+                v0 -= ((v1 << U32(4) ^ v1 >> U32(5)) + v1) ^ \
+                      (sum + U32(k[sum & U32(3)]))
 
             ret += struct.pack("<I", v0) + struct.pack("<I", v1)
         return NetworkMessage(ret)
