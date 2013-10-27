efde332b57f155836497279d216bc911eb7f5fce
d33tah <d33tah@gmail.com>
2013-10-27 19:29:11 +0100

    Since I changed the way I'm testing the proxy, listen on 7171 and connect to 7172.

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 # The server address and port number the proxy has to connect to.
 destination_host = '127.0.0.1'
-destination_port = '7171'
+destination_port = '7172'
 
 # The server address and port number the proxy will listen on.
 listen_host = '127.0.0.1'
-listen_port = '7172'
+listen_port = '7171'
 
 # The server address and port number that will be sent to the client.
 # Change it only if you're behind a NAT. Defaults to the listen address.
