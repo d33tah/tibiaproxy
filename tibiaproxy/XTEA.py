d33tah/tibiaproxy

commit 6b6f071a9010eca09b15127d08bdc5a5a56d3265

Various coding style fixes and documentation.

tibiaproxy/XTEA.py
@@ -1,20 +1,22 @@
 """
-This file is part of tibiaproxy.
-
-tibiaproxy is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-Joggertester is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Foobar; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+XTEA encryption/decryption module.
 """
+
+#This file is part of tibiaproxy.
+#
+#tibiaproxy is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#Joggertester is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Foobar; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 import struct
 
@@ -30,20 +32,20 @@ class U32:
             base: the base of the integer use if the num given was a string
         """
         if base is None:
-            self.int = int(num) % 2**32
+            self.int_ = int(num) % 2**32
         else:
-            self.int = int(num, base) % 2**32
+            self.int_ = int(num, base) % 2**32
 
-    def __coerce__(self, x):
+    def __coerce__(self, ignored):
         return None
 
     def __str__(self):
-        return "<U32 instance at 0x%x, int=%d>" % (id(self), self.int)
+        return "<U32 instance at 0x%x, int=%d>" % (id(self), self.int_)
 
-    def __getattr__(self, x):
+    def __getattr__(self, attribute_name):
         # you might want to take a look here:
         # http://stackoverflow.com/q/19611001/1091116
-        r = getattr(self.int, x)
+        r = getattr(self.int_, attribute_name)
         if callable(r):  # return a wrapper if integer's function was requested
             def f(*args, **kwargs):
                 if args and isinstance(args[0], U32):
@@ -51,7 +53,7 @@ class U32:
                 ret = r(*args, **kwargs)
                 if ret is NotImplemented:
                     return ret
-                if x in ['__str__', '__repr__', '__index__']:
+                if attribute_name in ['__str__', '__repr__', '__index__']:
                     return ret
                 ret %= 2**32
                 return U32(ret)
@@ -73,14 +75,14 @@ def XTEA_encrypt(buf, k):
         v0 = U32(struct.unpack("<I", buf[offset*8:offset*8+4])[0])
         v1 = U32(struct.unpack("<I", buf[offset*8+4:offset*8+8])[0])
         delta = U32(0x61C88647)
-        sum = U32(0)
+        sum_ = U32(0)
 
-        for i in range(32):
+        for _ in range(32):
             v0 += ((v1 << U32(4) ^ v1 >> U32(5)) + v1) ^ \
-                  (sum + U32(k[sum & U32(3)]))
-            sum -= delta
+                  (sum_ + U32(k[sum_ & U32(3)]))
+            sum_ -= delta
             v1 += ((v0 << U32(4) ^ v0 >> U32(5)) + v0) ^ \
-                  (sum + U32(k[sum >> U32(11) & U32(3)]))
+                  (sum_ + U32(k[sum_ >> U32(11) & U32(3)]))
 
         ret += struct.pack("<I", v0) + struct.pack("<I", v1)
     return bytearray(ret)
@@ -100,14 +102,14 @@ def XTEA_decrypt(buf, k):
         v0 = U32(struct.unpack("<I", buf[offset*8:offset*8+4])[0])
         v1 = U32(struct.unpack("<I", buf[offset*8+4:offset*8+8])[0])
         delta = U32(0x61C88647)
-        sum = U32(0xC6EF3720)
+        sum_ = U32(0xC6EF3720)
 
-        for i in range(32):
+        for _ in range(32):
             v1 -= ((v0 << U32(4) ^ v0 >> U32(5)) + v0) ^ \
-                  (sum + U32(k[sum >> U32(11) & U32(3)]))
-            sum += delta
+                  (sum_ + U32(k[sum_ >> U32(11) & U32(3)]))
+            sum_ += delta
             v0 -= ((v1 << U32(4) ^ v1 >> U32(5)) + v1) ^ \
-                  (sum + U32(k[sum & U32(3)]))
+                  (sum_ + U32(k[sum_ & U32(3)]))
 
         ret += struct.pack("<I", v0) + struct.pack("<I", v1)
     return bytearray(ret)

config.py
@@ -1,3 +1,6 @@
+"""This is tibiaproxy configuration file. Edit the lines below to configure
+tibiaproxy."""
+
 # Whether to use real Tibia (True) encryption or OpenTibia encryption (False).
 real_tibia = False
 

main.py
@@ -24,8 +24,8 @@ import importlib
 import os
 from tibiaproxy.util import log
 
-if __name__ == '__main__':
-
+def main():
+    """tibiaproxy's entry point."""
     plugins = []
     for filename in os.listdir('plugins'):
         if filename == "__init__.py" or filename[-3:] != ".py":
@@ -34,7 +34,7 @@ if __name__ == '__main__':
         plugins += [importlib.import_module('plugins.' + plugin_name)]
         log("Loaded plugin %s." % plugin_name)
 
-    s = Server(destination_login_host=config.destination_login_host,
+    server = Server(destination_login_host=config.destination_login_host,
                destination_login_port=config.destination_login_port,
                listen_login_host=config.listen_login_host,
                listen_login_port=config.listen_login_port,
@@ -45,4 +45,7 @@ if __name__ == '__main__':
                real_tibia=config.real_tibia,
                debug=config.debug,
                plugins=plugins)
-    s.run()
+    server.run()
+
+if __name__ == '__main__':
+    main()

plugins/eval.py
@@ -1,4 +1,13 @@
+"""eval.py
+
+Whenever the user says anything that begins with >, it gets executed as
+a line of Python code, with the result being sent back to the player.
+
+The original message does not get forwarded to the server.
+"""
+
 def on_client_say(conn, msg):
+    """If the message started with >, run it as a Python code."""
     if not msg.startswith(">"):
         return False
     try:

plugins/hello.py
@@ -1,4 +1,12 @@
+"""hello.py
+
+If the user says "hello world", a message "yeah, hi" is sent back to him.
+
+The original message does not get forwarded to the server.
+"""
+
 def on_client_say(conn, msg):
+    """Send "yeah, hi" if user said "hello world"."""
     if msg != "hello world":
         return False
 

tibiaproxy/GameProtocol.py
@@ -2,23 +2,21 @@
 GameProtocol.py - contains code needed to handle the game protocol.
 """
 
-"""
-This file is part of tibiaproxy.
-
-tibiaproxy is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-Joggertester is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Foobar; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-"""
+# This file is part of tibiaproxy.
+#
+# tibiaproxy is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 2 of the License, or
+# (at your option) any later version.
+#
+# Joggertester is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with Foobar; if not, write to the Free Software
+# Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 from NetworkMessage import NetworkMessage, adlerChecksum
 import RSA
@@ -26,12 +24,36 @@ import struct
 
 
 def create_handshake_challenge(timestamp, random_number):
+    """Create a handshake challenge dictionary based on the given parameters.
+
+    Args:
+        timestamp (int): the challenge's timestamp
+        random_number (int): the challenge random number
+    Returns dict
+    """
     return {'timestamp': timestamp, 'random_number': random_number}
 
 
 def create_handshake_reply(xtea_key, account_number, password, character_name,
                            timestamp, random_number, challenge_pos, first_16,
                            decrypted_raw):
+    """Create a handshake reply dictionary. This is the first packet the client
+    sends to the game server.
+
+    Args:
+        xtea_key (int): the XTEA key used in further communication
+        account_number (string): the account ID
+        password (string): the account password
+        character_name (string): the character to be logged in
+        timestamp (int) the challenge's timestamp response
+        random_number (int): the challenge's random number response
+        challenge_pos (int): the offset at which the challenge can be found
+        first_16 (int): the first 16 bytes of the original message
+        decrypted_raw (bytearray): the unencrypted original message
+
+    Returns dict
+    """
+    # FIXME: decrypted_raw feels odd. There should be a better way.
     return {'xtea_key': xtea_key,
             'account_number': account_number,
             'password': password,
@@ -70,7 +92,7 @@ def parseFirstMessage(orig_msg):
     msg_buf = RSA.RSA_decrypt(orig_msg.getRest()[:128])
     msg = NetworkMessage(msg_buf)
     # Extract the XTEA keys from the RSA-decrypted message.
-    xtea_key = [msg.getU32() for i in range(4)]
+    xtea_key = [msg.getU32() for _ in range(4)]
     assert(msg.getByte() == 0)        # gamemaster flag
     account_number = msg.getString()  # account
     character_name = msg.getString()  # character name
@@ -90,6 +112,15 @@ def parseFirstMessage(orig_msg):
 
 
 def prepareReply(handshake_reply, real_tibia):
+    """Create a handshake reply based on the dictionary from the argument that
+    has a modified challenge response.
+
+    handshake_reply (dict): the origina handshake reply dictionary
+    real_tibia (bool): whether to reencrypt the message for real Tibia
+
+    Returns bytearray
+    """
+
     to_encrypt_raw = handshake_reply['decrypted_raw']
     to_encrypt_msg = NetworkMessage(to_encrypt_raw)
     to_encrypt_msg.skipBytes(handshake_reply['challenge_pos'])

tibiaproxy/LoginProtocol.py
@@ -2,23 +2,21 @@
 LoginProtocol.py - contains code needed to handle the login protocol.
 """
 
-"""
-This file is part of tibiaproxy.
-
-tibiaproxy is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-Joggertester is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Foobar; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-"""
+#This file is part of tibiaproxy.
+#
+#tibiaproxy is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#Joggertester is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Foobar; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 from NetworkMessage import NetworkMessage
 import RSA
@@ -51,7 +49,7 @@ def parseFirstMessage(msg):
     msg_buf = RSA.RSA_decrypt(msg.getRest()[:128])
     msg = NetworkMessage(msg_buf)
     # Extract the XTEA keys from the RSA-decrypted message.
-    return [msg.getU32() for i in range(4)]
+    return [msg.getU32() for _ in range(4)]
 
 
 def parseReply(msg, xtea_key):
@@ -83,7 +81,7 @@ def parseReply(msg, xtea_key):
 
     num_worlds = msg.getByte()
     worlds = []
-    for i in range(num_worlds):
+    for _ in range(num_worlds):
         world_id = msg.getByte()
         world_name = msg.getString()
         world_hostname = msg.getString()
@@ -96,7 +94,7 @@ def parseReply(msg, xtea_key):
 
     num_chars = msg.getByte()
     characters = []
-    for i in range(num_chars):
+    for _ in range(num_chars):
         world_num = msg.getByte()
         char_world = worlds[world_num]
         char_name = msg.getString()

tibiaproxy/NetworkMessage.py
@@ -1,25 +1,22 @@
-"""
-A utility class used to extract structures out of network messages and build
+"""A utility class used to extract structures out of network messages and build
 custom ones.
 """
 
-"""
-This file is part of tibiaproxy.
-
-tibiaproxy is free software; you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation; either version 2 of the License, or
-(at your option) any later version.
-
-Joggertester is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with Foobar; if not, write to the Free Software
-Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
-"""
+#This file is part of tibiaproxy.
+#
+#tibiaproxy is free software; you can redistribute it and/or modify
+#it under the terms of the GNU General Public License as published by
+#the Free Software Foundation; either version 2 of the License, or
+#(at your option) any later version.
+#
+#Joggertester is distributed in the hope that it will be useful,
+#but WITHOUT ANY WARRANTY; without even the implied warranty of
+#MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+#GNU General Public License for more details.
+#
+#You should have received a copy of the GNU General Public License
+#along with Foobar; if not, write to the Free Software
+#Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
 
 import struct
 import XTEA
@@ -42,7 +39,7 @@ def adlerChecksum(buf):
     while length > 0:
         tmp = 5552 if length > 5552 else length
         length -= tmp
-        for i in reversed(range(tmp)):
+        for _ in reversed(range(tmp)):
             a += buf[pos]
             b += a
             pos += 1
@@ -112,7 +109,6 @@ class NetworkMessage:
 
         Returns None
         """
-        ret = self.buf[self.pos:self.pos+_bytes]
         self.pos += _bytes
 
     def getRest(self):
@@ -131,7 +127,7 @@ class NetworkMessage:
         ret = self.buf
         # Add the padding
         size = len(ret)
-        for i in range(8 - (size) % 8):
+        for _ in range(8 - (size) % 8):
             ret += "%c" % 0x33
 
         ret_with_size = struct.pack("<H", size) + ret

commit a4c527eb1a7878869e94041abeebe242f1b8ce5d

Fix the bug I introduced in 6b6f071a9010eca09b15127d08bdc5a5a56d3265.

tibiaproxy/XTEA.py
@@ -49,7 +49,7 @@ class U32:
         if callable(r):  # return a wrapper if integer's function was requested
             def f(*args, **kwargs):
                 if args and isinstance(args[0], U32):
-                    args = (args[0].int, ) + args[1:]
+                    args = (args[0].int_, ) + args[1:]
                 ret = r(*args, **kwargs)
                 if ret is NotImplemented:
                     return ret