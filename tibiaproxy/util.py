18289e0cc1ea21340d8c2d05628d0d90b84244cd
Jacek Wielemborek <d33tah@gmail.com>
2013-12-29 12:18:23 +0100

    Fix whitespace to util.py.

--- tibiaproxy/util.py
+++ tibiaproxy/util.py
@@ -57,6 +57,7 @@ def ip_to_u32(ip):
     """
     return struct.unpack("<I", socket.inet_aton(ip))[0]
 
+
 def assert_equal(v1, v2):
     if v1 != v2:
         sys.exit("assertion error: v1[%s] != v2[%s]" % (repr(v1), repr(v2)))
